86bbd12b7fe650a4d342fff8bc59bd755fce6c44
Thomas O. Bender <thobep21@hotmail.com>
2023-09-21 21:13:58 -0300

    Criação da tela da calculadora

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author 04396
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setTitle("Calculadora");
+        calc.setLocationRelativeTo(calc);
+        calc.setVisible(true);
+    }
+}
